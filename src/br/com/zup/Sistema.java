63db97571f69f96d9c1b404faeb0d7f9893b20e2
Raquel Leão <vc.raquel@gmail.com>
2021-09-29 10:44:19 -0300

    adicionando método para exibir menu na classe Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -8,4 +8,12 @@ public class Sistema {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+    //método para exibir menu
+    public static void menu() {
+        System.out.println("Bem vinde à taberna do Pôney Saltitante");
+
+        System.out.println("Digite 1 - Para exibir o cardápio");
+        System.out.println("Digite 2 - Para cadastrar um prato");
+        System.out.println("Digite 3 - Para sair");
+    }
 }

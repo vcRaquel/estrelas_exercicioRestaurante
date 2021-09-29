ee52954a669dd84ab41d0be49d2ed6b8ecfeba35
Raquel Leão <vc.raquel@gmail.com>
2021-09-29 10:45:08 -0300

    adicionando método para criar cadastrar ingredientes

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -8,6 +8,7 @@ public class Sistema {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+
     //método para exibir menu
     public static void menu() {
         System.out.println("Bem vinde à taberna do Pôney Saltitante");
@@ -15,5 +16,13 @@ public class Sistema {
         System.out.println("Digite 1 - Para exibir o cardápio");
         System.out.println("Digite 2 - Para cadastrar um prato");
         System.out.println("Digite 3 - Para sair");
+    }
+
+    //método para criar cadastrar ingredientes:
+    public static Ingrediente cadastrarIngrediente() {
+        String nomeIngrediente = capturarDados("Digite o nome do ingrediente: ").nextLine();
+        String quantidadeIngrediente = capturarDados("Digite a quantidade do Ingrediente: ").nextLine();
+        Ingrediente ingrediente = new Ingrediente(nomeIngrediente,quantidadeIngrediente);
+        return ingrediente;
     }
 }

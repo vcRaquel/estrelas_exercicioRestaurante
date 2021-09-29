eb95421bd825f4240c7aee1257766731376feee6
Raquel Leão <vc.raquel@gmail.com>
2021-09-29 10:39:57 -0300

    criando classe Restaurante

---
+++ src/br/com/zup/Restaurante.java
@@ -0,0 +1,32 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurante {
+    List<Prato> cardapio = new ArrayList<>();
+
+    //Método construtor
+    public Restaurante() {
+
+    }
+
+    //Getter
+    public List<Prato> getCardapio() {
+        return cardapio;
+    }
+
+    //Método para adicionar um prato
+    public void adicionarPrato(Prato novoPrato){
+        cardapio.add(novoPrato);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Quantidade de pratos: " + cardapio.size());
+        retorno.append("Pratos: ");
+        retorno.append("\n" + cardapio);
+        return retorno.toString();
+    }
+}
